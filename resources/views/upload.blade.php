f0d35c1ff1851c75abc11ec37dc4401ff2863212
DESKTOP-U9B2P9O\BUTT <zaynbutt143@gmail.com>
2024-10-08 10:48:12 +0500

    seeder

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -7,16 +7,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light px-4">
   <a class="navbar-brand" href="#">Customer Management System</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="{{ route('profile.edit') }}">Profile</a>
-      </li>
+  <ul class="navbar-nav ms-auto"> 
+      
       <li class="nav-item">
         <a class="nav-link" href="{{ route('logout') }}">Logout</a>
       </li>
